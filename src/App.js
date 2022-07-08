1a6d52855888e04cb2a5d095e3a18814ba348fea
Samuel Png <samuelpng7@gmail.com>
2022-07-08 06:48:03 +0000

    added function to change display page according to icon pressed

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import logo from './pictures/sgbirds-logo.png';
 export default class App extends React.Component {
 
   state = {
-    home: false,
+    home: true,
     explore: false,
     add: false,
     map: false,
@@ -46,60 +46,76 @@ export default class App extends React.Component {
     })
   }
 
+  pageToRender = () => {
+    if (this.state.home) {
+      return (
+        <React.Fragment>
+          <div className="header">
+            <img src={logo} alt="logo" height="90px" />
+          </div>
+          <div style={{height:"90px"}}></div>
+          <div><h1>Home Page</h1></div>
+        </React.Fragment>)
+    } else if (this.state.explore) {
+      return (<h1>explore</h1>)
+    } else if (this.state.add) {
+      return (<h1>add</h1>)
+    } else if (this.state.map) {
+      return (<h1>Map</h1>)
+    } else {
+      return (<h1>Profile</h1>)
+    }
+  }
+
   render() {
     return (
       <React.Fragment>
         <body>
-        <div className="header">
-          <img src={logo} alt="logo" height="90px"/>
-        </div>
-
-       <div>
-        
-       </div>
-
-        
-
-        <div className="container">
-          <div className="navigation">
-            <ul>
-              <li onClick={this.homeIcon}>
-                <div className="icon-container">
-                  <span className={this.state.home ? "iconActive" : "iconInactive"}><IoHomeOutline /></span>
-                  <span className={this.state.home ? "textActive" : "textInactive"}>Home</span>
-                </div>
-              </li>
-              <li onClick={this.exploreIcon}>
-                <div className="icon-container">
-                  <span className={this.state.explore ? "iconActive" : "iconInactive"}><BiSearchAlt /></span>
-                  <span className={this.state.explore ? "textActive" : "textInactive"}>Explore</span>
-                </div>
-              </li>
-              <li onClick={this.addIcon}>
-                <div className="icon-container">
-                  <span className={this.state.add ? "iconActive" : "iconInactive"}><BiPlusCircle /></span>
-                  <span className={this.state.add ? "textActive" : "textInactive"}>Add</span>
-                </div>
-              </li>
-              <li onClick={this.mapIcon}>
-                <div className="icon-container">
-                  <span className={this.state.map ? "iconActive" : "iconInactive"}><BiMap /></span>
-                  <span className={this.state.map ? "textActive" : "textInactive"}>Map</span>
-                </div>
-              </li>
-              <li onClick={this.profileIcon}>
-                <div className="icon-container">
-                  <span className={this.state.profile ? "iconActive" : "iconInactive"}><IoPersonOutline /></span>
-                  <span className={this.state.profile ? "textActive" : "textInactive"}>Profile</span>
-                </div>
-              </li>
-              {/* {this.state.indicator ? <div className="indicator"></div> : <div></div>} */}
-            </ul>
+          <div className="main">
+            {this.pageToRender()}
           </div>
-        </div>
+
+          <div className="container">
+            <div className="navigation">
+              <ul>
+                <li onClick={this.homeIcon}>
+                  <div className="icon-container">
+                    <span className={this.state.home ? "iconActive" : "iconInactive"}><IoHomeOutline /></span>
+                    <span className={this.state.home ? "textActive" : "textInactive"}>Home</span>
+                  </div>
+                </li>
+                <li onClick={this.exploreIcon}>
+                  <div className="icon-container">
+                    <span className={this.state.explore ? "iconActive" : "iconInactive"}><BiSearchAlt /></span>
+                    <span className={this.state.explore ? "textActive" : "textInactive"}>Explore</span>
+                  </div>
+                </li>
+                <li onClick={this.addIcon}>
+                  <div className="icon-container">
+                    <span className={this.state.add ? "iconActive" : "iconInactive"}><BiPlusCircle /></span>
+                    <span className={this.state.add ? "textActive" : "textInactive"}>Add</span>
+                  </div>
+                </li>
+                <li onClick={this.mapIcon}>
+                  <div className="icon-container">
+                    <span className={this.state.map ? "iconActive" : "iconInactive"}><BiMap /></span>
+                    <span className={this.state.map ? "textActive" : "textInactive"}>Map</span>
+                  </div>
+                </li>
+                <li onClick={this.profileIcon}>
+                  <div className="icon-container">
+                    <span className={this.state.profile ? "iconActive" : "iconInactive"}><IoPersonOutline /></span>
+                    <span className={this.state.profile ? "textActive" : "textInactive"}>Profile</span>
+                  </div>
+                </li>
+                {/* {this.state.indicator ? <div className="indicator"></div> : <div></div>} */}
+              </ul>
+            </div>
+          </div>
         </body>
       </React.Fragment>
     )
   }
 }
+
 
