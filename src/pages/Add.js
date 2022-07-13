deef4d21b7a78985b48903bf2d9fab2b8ae670e6
Samuel Png <samuelpng7@gmail.com>
2022-07-13 16:18:45 +0000

    added design changes to birdSize and birdColours in form for add new

--- src/pages/Add.js
+++ src/pages/Add.js
@@ -3,17 +3,45 @@ import axios from 'axios';
 // import DatePicker from "react-datepicker";
 import '../App.css';
 import Select from 'react-select';
+import sparrow from '../pictures/sparrow.png';
+import blackbird from '../pictures/blackbird.png';
+import crow from '../pictures/crow.png';
+import goose from '../pictures/goose.png'
 // import { AccordionCollapse } from 'react-bootstrap';
 // import LocationPicker from "react-leaflet-location-picker";
 
 const options = [
-    { value: 'red', label: 'Red' },
-    { value: 'blue', label: 'Blue' },
-    { value: 'green', label: 'Green' },
-    { value: 'yellow', label: 'Yellow' },
-    { value: 'brown', label: 'Brown' },
-    { value: 'white', label: 'White' },
+    { value: 'black', label: 'Black', color: 'black'},
+    { value: 'grey', label: 'Grey', color: 'grey'},
+    { value: 'white', label: 'White', color: 'lightgrey' },
+    { value: 'brown', label: 'Brown', color: 'brown' },
+    { value: 'red', label: 'Red', color: 'red' },
+    { value: 'blue', label: 'Blue', color: 'blue' },
+    { value: 'green', label: 'Green', color: 'green' },
+    { value: 'yellow', label: 'Yellow', color: 'goldenrod' },
+    { value: 'orange', label: 'Orange', color: 'orange'}   
 ];
+
+const colorStyles = {
+    control: (styles) => ({...styles, backgroundColor: "white"}),
+    option: (styles, {data, isDisabled, isFocused, isSelected}) => {
+        return {...styles, color: data.color}
+    },
+    multiValue: (styles, {data}) => {
+        return {
+            ...styles,
+            backgroundColor: data.color,
+            color: 'white'
+        }
+    },
+    multiValueLabel: (styles, {data}) => {
+        return {
+            ...styles,
+            backgroundColor: data.color,
+            color: 'white'
+        }
+    }
+}
 
 
 export default class Add extends React.Component {
@@ -135,14 +163,20 @@ export default class Add extends React.Component {
                     <div className="row">
                         <div>
                             <div className="label mt-3">Bird Size</div>
-                            <div>small, sm, medium, ml, large</div>
-                            <input type="radio" className="ms-2" name="birdSize" value="1" key="1" onChange={this.updateFormField} checked={this.state.birdSize === "1"} />
-                            <input type="radio" className="ms-4" name="birdSize" value="2" key="2" onChange={this.updateFormField} checked={this.state.birdSize === "2"} />
-                            <input type="radio" className="ms-4" name="birdSize" value="3" key="3" onChange={this.updateFormField} checked={this.state.birdSize === "3"} />
-                            <input type="radio" className="ms-4" name="birdSize" value="4" key="4" onChange={this.updateFormField} checked={this.state.birdSize === "4"} />
-                            <input type="radio" className="ms-4" name="birdSize" value="5" key="5" onChange={this.updateFormField} checked={this.state.birdSize === "5"} />
-                            <input type="radio" className="ms-4" name="birdSize" value="6" key="6" onChange={this.updateFormField} checked={this.state.birdSize === "6"} />
-                            <input type="radio" className="ms-4" name="birdSize" value="7" key="7" onChange={this.updateFormField} checked={this.state.birdSize === "7"} />
+                            <div style={{height: '52px'}}></div>
+                            <div style={{position:'relative'}}>
+                                <label for="1" style={{position:'absolute', bottom:'0'}}><img src={sparrow}  alt="sparrow" height="15px" /></label>
+                                <label for="3" style={{position:'absolute', bottom:'0', left: '75px'}}><img src={blackbird} alt="blackbird" height="25px" /></label>
+                                <label for="5" style={{position:'absolute', bottom:'0', left: '143px'}}><img src={crow} alt="crow" height="35px" /></label>
+                                <label for="7" style={{position:'absolute', bottom:'0', left: '220px'}}><img src={goose} alt="goose" height="50px" /></label>
+                            </div>
+                            <input type="radio" className="ms-2" name="birdSize" value="1" key="1" id="1"onChange={this.updateFormField} checked={this.state.birdSize === "1"} />
+                            <input type="radio" className="ms-4" name="birdSize" value="2" key="2" id="2"onChange={this.updateFormField} checked={this.state.birdSize === "2"} />
+                            <input type="radio" className="ms-4" name="birdSize" value="3" key="3" id="3"onChange={this.updateFormField} checked={this.state.birdSize === "3"} />
+                            <input type="radio" className="ms-4" name="birdSize" value="4" key="4" id="4"onChange={this.updateFormField} checked={this.state.birdSize === "4"} />
+                            <input type="radio" className="ms-4" name="birdSize" value="5" key="5" id="5"onChange={this.updateFormField} checked={this.state.birdSize === "5"} />
+                            <input type="radio" className="ms-4" name="birdSize" value="6" key="6" id="6"onChange={this.updateFormField} checked={this.state.birdSize === "6"} />
+                            <input type="radio" className="ms-4" name="birdSize" value="7" key="7" id="7"onChange={this.updateFormField} checked={this.state.birdSize === "7"} />
                         </div>
                         <div>
                             <div className="label mt-3">Bird Family</div>
@@ -170,6 +204,7 @@ export default class Add extends React.Component {
                                     value={birdColours}
                                     onChange={this.handleChange}
                                     options={options}
+                                    styles={colorStyles}
                                 />
                             </div>
                         </div>
